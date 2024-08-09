90982469d2cd601a9a10f1ca2dcda4bc27b9efbb
unknown <124611058+Safuanov888@users.noreply.github.com>
2024-08-09 18:58:00 +0300

    Полностью закончил работу над сайтом и внедрил несколько ML моделей

--- DeployModel-Project/DeployModel/urls.py
+++ DeployModel-Project/DeployModel/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('result/', views.result, name='result')
+    path('result/', views.result, name='result'),
+    path('info/', views.info, name='info')
 ]
--- DeployModel-Project/DeployModel/views.py
+++ DeployModel-Project/DeployModel/views.py
@@ -1,121 +1,96 @@
 from django.shortcuts import render
 import joblib
+from django.templatetags.static import static
+
+
+def check_perhap(data):
+    if ',' in data:
+        return float(data.replace(',', '.'))
+    else:
+        return float(data)
+
+
+def create_answer(pred):
+    if pred[0] == 1:
+        return 'Модель предсказала, что клиент уйдёт в отток'
+    else:
+        return 'Модель предсказала, что клиент не уйдёт в отток'
+
+
+def get_number(x):
+    if x == '1':
+        return 1
+    else:
+        return 0
+
+
+def get_model(model_name):
+    if model_name == 'Логистическая регрессия':
+        return [joblib.load('LogisticRegression.joblib'), static('images/LogisticRegression_1.png'),
+                static('images/LogisticRegression_2.png'), static('images/LogisticRegression_3.png')]
+    elif model_name == 'Случайный лес':
+        return [joblib.load('RandomForest.joblib'), static('images/RandomForest_1.png'),
+                static('images/RandomForest_2.png'), static('images/RandomForest_3.png')]
+    else:
+        return [joblib.load('AdaBoosting.joblib'), static('images/AdaBoosting_1.png'),
+                static('images/AdaBoosting_2.png'), static('images/AdaBoosting_3.png')]
+
 
 def home(request):
     return render(request, 'home.html')
 
+
+def info(request):
+    return render(request, 'info.html')
+
+
 def result(request):
-    def get_number(x):
-        if x == '1':
-            return 1
-        else:
-            return 0
-
-    model = joblib.load('AdaBoosting.joblib')
-    scaler = joblib.load('scaler.save')
-    data = [0] * 33
-    gender = request.GET['Гендер человека']
-    data[3] = get_number(gender)
-
-    senior = request.GET['Пенсионер']
-    data[4] = get_number(senior)
-
-    married = request.GET['Женат/замужем']
-    data[5] = get_number(married)
-
-    children = request.GET['Есть дети']
-    data[6] = get_number(children)
+    model_name = request.GET['Модель']
+    model, image_1, image_2, image_3 = get_model(model_name)  # Загрузка модели и графиков обучения
 
     tenure = int(request.GET['Длительность контракта (месяцы)'])
+    buy_month = check_perhap(request.GET['Ежемесячные траты на пользование интернетом'])
+    buy_total = check_perhap(request.GET['Общие траты за весь период пользования интернетом'])
+    scaler = joblib.load('scaler.save')
+    transformed_data = scaler.transform(
+        [[tenure, buy_month, buy_total]])  # Преобразование численных переменных с помощью scaler
 
-    dom_phone = request.GET['Подписка на домашний телефон компании']
-    data[7] = get_number(dom_phone)
+    data = [0] * 33  # Количество признаков в массиве для предсказания
+    data[0] = transformed_data[0][0]
+    data[1] = transformed_data[0][1]
+    data[2] = transformed_data[0][2]
+    data[3] = get_number(request.GET['Гендер человека'])
+    data[4] = get_number(request.GET['Пенсионер'])
+    data[5] = get_number(request.GET['Женат/замужем'])
+    data[6] = get_number(request.GET['Есть дети'])
+    data[7] = get_number(request.GET['Подписка на домашний телефон компании'])
+    data[8] = 1 if request.GET['Подписка на несколько телефонных линий компании'] == '2' else 0
+    data[9] = 1 if request.GET['Подписка на несколько телефонных линий компании'] == '1' else 0
+    data[10] = 1 if request.GET['Интернет услуга компании'] == '3' else 0
+    data[11] = 1 if request.GET['Интернет услуга компании'] == '1' else 0
+    data[12] = 1 if request.GET['Подписка на онлайн безопасность'] == '2' else 0
+    data[13] = 1 if request.GET['Подписка на онлайн безопасность'] == '1' else 0
+    data[14] = 1 if request.GET['Подписка на резервное копирование'] == '2' else 0
+    data[15] = 1 if request.GET['Подписка на резервное копирование'] == '1' else 0
+    data[16] = 1 if request.GET['Подписка на дополнительную онлайн безопасность'] == '2' else 0
+    data[17] = 1 if request.GET['Подписка на дополнительную онлайн безопасность'] == '1' else 0
+    data[18] = 1 if request.GET['Подписка на техническую поддержку'] == '2' else 0
+    data[19] = 1 if request.GET['Подписка на техническую поддержку'] == '1' else 0
+    data[20] = 1 if request.GET['Подписка на потоковую передачу телевезионных программ'] == '2' else 0
+    data[21] = 1 if request.GET['Подписка на потоковую передачу телевезионных программ'] == '1' else 0
+    data[22] = 1 if request.GET['Подписка на трансляцию фильмов'] == '2' else 0
+    data[23] = 1 if request.GET['Подписка на трансляцию фильмов'] == '1' else 0
+    data[24] = 1 if request.GET['Тип контракта'] == '2' else 0
+    data[25] = 1 if request.GET['Тип контракта'] == '3' else 0
+    data[26] = get_number(request.GET['Подписка на онлайн оплату'])
+    data[27] = 1 if request.GET['Способ оплаты интернета'] == '4' else 0
+    data[28] = 1 if request.GET['Способ оплаты интернета'] == '1' else 0
+    data[29] = 1 if request.GET['Способ оплаты интернета'] == '2' else 0
+    data[30] = 1 if 12 < tenure <= 24 else 0
+    data[31] = 1 if 24 < tenure <= 48 else 0
+    data[32] = 1 if tenure > 48 else 0
 
-    couple_lines = request.GET['Подписка на несколько телефонных линий компании']
-    if couple_lines == '1':
-        data[9] = 1
-    elif couple_lines == '2':
-        data[8] = 1
+    ans = create_answer(model.predict([data]))
 
-    service = request.GET['Интернет услуга компании']
-    if service == '1':
-        data[11] = 1
-    elif service == '3':
-        data[10] = 1
-
-    security = request.GET['Подписка на онлайн безопасность']
-    if security == '1':
-        data[13] = 1
-    elif security == '2':
-        data[12] = 1
-
-    copy = request.GET['Подписка на резервное копирование']
-    if copy == '1':
-        data[15] = 1
-    elif copy == '2':
-        data[14] = 1
-
-    ad_security = request.GET['Подписка на дополнительную онлайн безопасность']
-    if ad_security == '1':
-        data[17] = 1
-    elif ad_security == '2':
-        data[16] = 1
-
-    tech_security = request.GET['Подписка на техническую поддержку']
-    if tech_security == '1':
-        data[19] = 1
-    elif tech_security == '2':
-        data[18] = 1
-
-    flaw = request.GET['Подписка на потоковую передачу телевезионных программ']
-    if flaw == '1':
-        data[21] = 1
-    elif flaw == '2':
-        data[20] = 1
-
-    translation = request.GET['Подписка на трансляцию фильмов']
-    if translation == '1':
-        data[23] = 1
-    elif translation == '2':
-        data[22] = 1
-
-    contract = request.GET['Тип контракта']
-    if contract == '2':
-        data[24] = 1
-    elif contract == '3':
-        data[25] = 1
-
-    paperless = request.GET['Подписка на онлайн оплату']
-    data[26] = get_number(paperless)
-
-    buy_type = request.GET['Способ оплаты интернета']
-    if buy_type == '1':
-        data[28] = 1
-    elif buy_type == '2':
-        data[29] = 1
-    elif buy_type == '4':
-        data[27] = 1
-
-    buy_month = request.GET['Ежемесячные траты на пользование интернетом']
-    if ',' in buy_month:
-        buy_month = float(buy_month.replace(',', '.'))
-    buy_total = request.GET['Общие траты за весь период пользования интернетом']
-    if ',' in buy_total:
-        buy_total = float(buy_total.replace(',', '.'))
-
-    transform_data = scaler.transform([[tenure, buy_month, buy_total]])
-    data[0] = transform_data[0][0]
-    data[1] = transform_data[0][1]
-    data[2] = transform_data[0][2]
-
-    if 12 < tenure <= 24:
-        data[30] = 1
-    elif 24 < tenure <= 48:
-        data[31] = 1
-    elif tenure > 48:
-        data[32] = 1
-
-    ans = model.predict([data])
-
-
-    return render(request, 'result.html', {'ans': ans[0]})
+    return render(request, 'result.html',
+                  {'ans': ans, 'model_name': model_name, 'image_1': image_1, 'image_2': image_2, 'image_3': image_3})
